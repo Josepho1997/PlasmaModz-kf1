c72e627c7eec33e001c3acd1f2e71ba6f8037bb4
Joseph <josephoneill1997@gmail.com>
2013-03-30 00:01:23 -0400

    Added onListItemClick
    I added an onListItemClick, as well as more boot animations

--- src/com/cydeon/plasmamodz/Boots1.java
+++ src/com/cydeon/plasmamodz/Boots1.java
@@ -23,16 +23,34 @@ public class Boots1 extends ListActivity {
 	 
 		@Override
 		protected void onListItemClick(ListView l, View v, int position, long id) {
-	 
-			//get selected items
+			
 			String selectedValue = (String) getListAdapter().getItem(position);
 			Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+		    Intent a = new Intent(Boots1.this, BootAnimation.class);
 			 if(position == 0){
-				 Intent a = new Intent(Boots1.this, Softkeys.class);
+				 a.putExtra("Android", "android");
 				 startActivity(a);
 			 }else if(position == 1){
-				 Intent b = new Intent(Boots1.this, Themes.class);
-				 startActivity(b);
+				 a.putExtra("Dragon", "dragon");
+				 startActivity(a);
+			 }else if (position == 2){
+				 a.putExtra("GameBoy", "gameboy");
+				 startActivity(a);
+			 }else if (position == 3){
+				 a.putExtra("GameCube", "gamecube");
+				 startActivity(a);
+			 }else if (position == 4){
+				 a.putExtra("Nexus", "nexus");
+				 startActivity(a);
+			 }else if (position == 5){
+				 a.putExtra("Nightmare", "nightmare");
+				 startActivity(a);
+			 }else if (position == 6){
+				 a.putExtra("Xbox", "xbox");
+				 startActivity(a);
+			 }else if (position == 7){
+				 a.putExtra("Xbox1", "xbox1");
+				 startActivity(a);
 			 }
 	 
 		}
