460bde2a5569c79ad6b0008bd366eb173871cde3
Joseph <josephoneill1997@gmail.com>
2013-04-09 23:50:45 -0400

    Added framework-res compatibility

--- src/com/cydeon/plasmamodz/Install.java
+++ src/com/cydeon/plasmamodz/Install.java
@@ -1,10 +1,16 @@
 package com.cydeon.plasmamodz;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.concurrent.TimeoutException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 import com.stericson.RootTools.*;
 import com.stericson.RootTools.exceptions.RootDeniedException;
@@ -36,6 +42,7 @@ public class Install extends Activity implements Runnable{
 			@Override
 			public void onClick(View arg0) {
 				showDialog();
+				
 			}
 		});
 	}
@@ -50,7 +57,7 @@ public class Install extends Activity implements Runnable{
 	public void run(){
 		try{
 			Thread.sleep(1000);
-			CommandCapture command = new CommandCapture(0, "su", "#!/system/bin/sh", "busybox mount -o remount, rw /system", "rm -r /sdcard/plasma/working", "wait", "cd /sdcard/plasma", "mkdir working", "mkdir mods", "wait", "mv /sdcard/Download/batterymod.zip /sdcard/plasma/mods/", "mv /sdcard/Download/softkey.zip /sdcard/plasma/mods/", "mv /sdcard/Download/wifi.zip /sdcard/plasma/mods/", "mv /sdcard/Download/dp.zip /sdcard/plasma/", "mv /sdcard/Download/stat.zip /sdcard/plasma/mods/", "unzip -o /sdcard/plasma/mods/batterymod.zip -d /sdcard/plasma/working", "wait", "unzip -o /sdcard/plasma/mods/wifi.zip -d /sdcard/plasma/working", "wait", "unzip -o /sdcard/plasma/mods/softkey.zip -d /sdcard/plasma/working", "unzip -o /sdcard/plasma/dp.zip -d /sdcard/", "unzip -o /sdcard/plasma/mods/stat.zip -d /sdcard/plasma/working", "wait", "cd working", "cd SystemUI", "cp /system/app/SystemUI.apk /sdcard/plasma/working/SystemUI", "wait", "cd res", "cp -r /sdcard/layout /sdcard/plasma/working/SystemUI/res", "wait", "cp -r /sdcard/drawable /sdcard/plasma/working/SystemUI/res",  "wait", "cp -r /sdcard/layout-sw600dp /sdcard/plasma/working/SystemUI/res", "wait",  "cp /sdcard/plasma/zip /system/bin", "cp /sdcard/plasma/zipalign /system/bin", "chmod 777 /system/bin/zip", "chmod 777 /system/bin/zipalign", "cd /sdcard/plasma/working/SystemUI", "zip -r SystemUI.apk res", "wait", "mv SystemUI.apk SystemU.apk", "wait", "zipalign -v 4 SystemU.apk SystemUI.apk", "wait", "rm /sdcard/plasma/working/System/res/drawable/stat_sys_battery.xml", "wait", "cp SystemUI.apk /system/app", "wait", "wait", "chmod 644 /system/app/SystemUI.apk", "wait", "rm -r /sdcard/plasma/mods", "rm /sdcard/plasma/dp.zip", "rm -r /sdcard/plasma/mods", "rm -r /sdcard/plasma/working", "rm -r /sdcard/drawable", "rm -r /sdcard/layout", "rm -r /sdcard/layout-sw600dp", "busybox killall system_server");
+			CommandCapture command = new CommandCapture(0, "su", "#!/system/bin/sh", "busybox mount -o remount, rw /system", "rm -r /sdcard/plasma/working", "wait", "cd /sdcard/plasma", "mkdir working", "mkdir mods", "wait", "mv /sdcard/Download/batterymod.zip /sdcard/plasma/mods/", "mv /sdcard/Download/softkey.zip /sdcard/plasma/mods/", "mv /sdcard/Download/wifi.zip /sdcard/plasma/mods/", "mv /sdcard/Download/dp.zip /sdcard/plasma/", "mv /sdcard/Download/stat.zip /sdcard/plasma/mods/", "unzip -o /sdcard/plasma/mods/batterymod.zip -d /sdcard/plasma/working", "wait", "unzip -o /sdcard/plasma/mods/wifi.zip -d /sdcard/plasma/working", "wait", "unzip -o /sdcard/plasma/mods/softkey.zip -d /sdcard/plasma/working", "unzip -o /sdcard/plasma/dp.zip -d /sdcard/", "unzip -o /sdcard/plasma/mods/stat.zip -d /sdcard/plasma/working", "mv /sdcard/Download/lock.zip /sdcard/plasma/mods/", "wait",  "unzip -o /sdcard/plasma/mods/lock.zip -d /sdcard/plasma/working", "wait", "wait", "cd working", "cd SystemUI", "cp /system/app/SystemUI.apk /sdcard/plasma/working/SystemUI", "cp /system/framework/framework-res.apk /sdcard/plasma/working/framework-res",  "wait", "cd res", "cp -r /sdcard/layout /sdcard/plasma/working/SystemUI/res", "wait", "cp -r /sdcard/drawable /sdcard/plasma/working/SystemUI/res",  "wait", "cp -r /sdcard/layout-sw600dp /sdcard/plasma/working/SystemUI/res", "wait", "cd /sdcard/plasma/working/SystemUI", "rm /sdcard/plasma/working/System/res/drawable/stat_sys_battery.xml", "zip -r SystemUI.apk res", "wait", "cd /sdcard/plasma/working/framework-res", "zip -r framework-res.apk res", "wait", "cd /sdcard/plasma/working/SystemUI", "mv SystemUI.apk SystemU.apk", "wait", "zipalign -v 4 SystemU.apk SystemUI.apk", "wait", "cp SystemUI.apk /system/app", "wait", "cd /sdcard/plasma/working/framework-res", "mv framework-res.apk frame.apk", "wait", "zipalign -v 4 frame.apk framework-res.apk", "wait", "cp framework-res.apk /system/framework", "wait", "chmod 644 /system/app/SystemUI.apk", "wait", "chmod 644 /system/framework/framework-res.apk", "rm -r /sdcard/plasma/mods", "rm /sdcard/plasma/dp.zip", "rm -r /sdcard/plasma/mods", "wait", "rm -r /sdcard/drawable", "wait", "rm -r /sdcard/layout", "wait", "rm -r /sdcard/layout-sw600dp", "rm -r /sdcard/plasma/working", "busybox killall system_server");
 			try {
 				RootTools.getShell(true).add(command).waitForFinish();
 			} catch (InterruptedException e) {
@@ -74,4 +81,5 @@ public class Install extends Activity implements Runnable{
 		}
 	};
 	
+
 }
