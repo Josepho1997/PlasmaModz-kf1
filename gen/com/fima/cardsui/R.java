1e875479fe8869cbbf1dcdafcc67369ecb8dcd73
Joseph <josephoneill1997@gmail.com>
2013-05-03 22:03:37 -0400

    Updated Gen Files

--- gen/com/fima/cardsui/R.java
+++ gen/com/fima/cardsui/R.java
@@ -10,12 +10,12 @@ public final class R {
 	public static final class id {
 		public static final int listView = 0x7f0b000e;
 		public static final int sticky = 0x7f0b000f;
-		public static final int cardContent = 0x7f0b001e;
+		public static final int cardContent = 0x7f0b001f;
 		public static final int placeholder = 0x7f0b0013;
 		public static final int tableLayout = 0x7f0b0011;
-		public static final int stackContainer = 0x7f0b0020;
+		public static final int stackContainer = 0x7f0b0021;
 		public static final int ScrollView1 = 0x7f0b0010;
-		public static final int stackTitle = 0x7f0b001f;
+		public static final int stackTitle = 0x7f0b0020;
 	}
 	public static final class style {
 		public static final int CardLightText = 0x7f070001;
